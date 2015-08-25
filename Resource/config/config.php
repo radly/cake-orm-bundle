614d18ea39bc4bfab5202a167d6a649cccaa68d8
Omid Mottaghi <omidmr@gmail.com>
2015-08-25 13:55:54 +0430

    add cakecore to config

--- Resource/config/config.php
+++ Resource/config/config.php
@@ -21,14 +21,16 @@ return [
                 'className' => 'File',
                 'path' => CACHE_DIR . DS . 'cake',
             ],
-            /**
-             * Configure the cache for model and datasource caches. This cache
-             * configuration is used to store schema descriptions, and table listings
-             * in connections.
-             */
+            '_cake_core_' => [
+                'className' => 'File',
+                'prefix' => 'radphp_cake_core_',
+                'path' => CACHE_DIR . DS . 'cake' . DS . 'persistent',
+                'serialize' => true,
+                'duration' => '+2 minutes',
+            ],
             '_cake_model_' => [
                 'className' => 'File',
-                'prefix' => 'myapp_cake_model_',
+                'prefix' => 'radphp_cake_model_',
                 'path' => CACHE_DIR . DS . 'cake' . DS . 'models',
                 'serialize' => true,
                 'duration' => '+2 minutes',
